e068c17e298e222fa27b6691aa7c3d53046ab596
Donny Hallman <donny.hallman@leidos.com>
2019-11-27 17:05:58 -0500

    Fixed missing cobertura configuration

--- karma.conf.js
+++ karma.conf.js
@@ -3,45 +3,45 @@
 
 module.exports = function(config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage"),
-      require("karma-coverage-istanbul-reporter"),
-      require("karma-junit-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma")
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage"),
-      reports: ["html", "lcovonly", "text-summary"],
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     preprocessors: {
-      "src/**/*.ts": ["coverage"]
+      'src/**/*.ts': ['coverage']
     },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessNoSandbox"],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"]
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     },
     singleRun: false,
-    reporters: ["progress", "kjhtml", "junit", "coverage-istanbul"],
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage-istanbul'],
     junitReporter: {
-      outputDir: "coverage",
+      outputDir: 'coverage',
       useBrowserName: false,
-      outputFile: "test-report.xml"
+      outputFile: 'test-report.xml'
     },
     restartOnFileChange: true
   });
