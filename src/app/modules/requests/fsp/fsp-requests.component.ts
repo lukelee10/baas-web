7aba4c1726eb9057905eb8ceed3376a9597849d0
Gopi Molakalapalli <gopi.r.molakalapalli@leidos.com>
2020-01-10 11:09:45 -0500

    integration-of-file-upload-todo-fix-lint-errors

--- src/app/modules/requests/fsp/fsp-requests.component.ts
+++ src/app/modules/requests/fsp/fsp-requests.component.ts
@@ -1,8 +1,5 @@
-import { HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { requireCheckboxesToBeCheckedValidator } from '../../../shared/require-checkboxes-to-be-checked.validator';
 
 @Component({
   selector: 'app-fsp-requests',
@@ -10,61 +7,10 @@ import { requireCheckboxesToBeCheckedValidator } from '../../../shared/require-c
 })
 export class FspRequestsComponent implements OnInit {
   form = new FormGroup({
-    packageTitle: new FormControl('', Validators.required),
-    selectClassification: new FormControl('', Validators.required),
-    vettingCheckBoxGroup: new FormGroup(
-      {
-        vettingLowBall: new FormControl(false),
-        vetting3LA: new FormControl(false),
-        vettingABIS: new FormControl(false),
-        vettingHIGHTOP: new FormControl(false)
-      },
-      requireCheckboxesToBeCheckedValidator()
-    )
+    packageTitle: new FormControl('', Validators.required)
   });
 
   constructor() {}
 
-  ngOnInit() {
-    this.form.get('packageTitle').setValue('');
-    console.log('packageTitle: ' + this.form.get('packageTitle').value);
-  }
-}
-
-export class RequestsComponent implements OnInit {
-  constructor() {}
-
-  classifications: classification[] = [
-    { value: 'U-0', viewValue: 'U' },
-    { value: 'U//FOUO-1', viewValue: 'U//FOUO' },
-    { value: 'U//LES-2', viewValue: 'Fingerprint' }
-  ];
-
-  @Input() httpRequestHeaders:
-    | HttpHeaders
-    | {
-        [header: string]: string | string[];
-      } = new HttpHeaders()
-    .set('sampleHeader', 'headerValue')
-    .set('sampleHeader1', 'headerValue1');
-
-  @Input()
-  httpRequestParams:
-    | HttpParams
-    | {
-        [param: string]: string | string[];
-      } = new HttpParams()
-    .set('sampleRequestParam', 'requestValue')
-    .set('sampleRequestParam1', 'requestValue1');
-
   ngOnInit() {}
-
-  public uploadEvent($event: any) {
-    console.log('from client' + JSON.stringify($event));
-  }
-}
-
-interface classification {
-  value: string;
-  viewValue: string;
 }
