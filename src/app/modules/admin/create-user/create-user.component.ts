f7fd53950f7d4db0755868c12339208932819349
Gopi Molakalapalli <gopi.r.molakalapalli@leidos.com>
2020-05-14 14:25:54 -0400

    loead-create-user-filtered-roles

--- src/app/modules/admin/create-user/create-user.component.ts
+++ src/app/modules/admin/create-user/create-user.component.ts
@@ -13,9 +13,13 @@ import {
   validateSpecialChar
 } from 'src/app/core/app-global-constants';
 import { AwsLambdaService } from 'src/app/core/services/aws-lambda.service';
+import { UserService } from 'src/app/core/services/user.service';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 
-import { LookupStaticDataService } from '../../../shared/services/lookup-static-data.service';
+import {
+  LookupStaticDataService,
+  SelectItem
+} from '../../../shared/services/lookup-static-data.service';
 import { GroupFlatNode } from '../group-management/group-management.component';
 
 @Component({
@@ -24,6 +28,7 @@ import { GroupFlatNode } from '../group-management/group-management.component';
 })
 export class CreateUserComponent implements OnInit {
   form: FormGroup;
+  userRolesArr: SelectItem[];
   get email() {
     return this.form.get('email');
   }
@@ -46,7 +51,8 @@ export class CreateUserComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     public lookupStaticDataService: LookupStaticDataService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private userService: UserService
   ) {}
 
   ngOnInit() {
@@ -68,6 +74,12 @@ export class CreateUserComponent implements OnInit {
       role: new FormControl('', [Validators.required]),
       disabled: new FormControl('', [Validators.required])
     });
+
+    this.userRolesArr = this.userService.IsAdmin
+      ? this.lookupStaticDataService.userRoleData
+      : this.lookupStaticDataService.userRoleData.filter(
+          role => role.value !== 'Admin'
+        );
   }
 
   submit() {
